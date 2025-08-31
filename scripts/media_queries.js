3d86d4f02d728340eaf7c46f39afed47ccefe9b3
Roxann BREMOND <roxannbremond@gmail.com>
2025-08-31 08:39:26 +0200

    Ajout du MediaQueries pour mobile

--- scripts/media_queries.js
+++ scripts/media_queries.js
@@ -11,10 +11,20 @@ const menu_principal = document.querySelectorAll(".navigation");
 navigation_principale.addEventListener("click", ()=>{
     const navigation_secondaire = document.getElementById("nav_secondaire");
     if (largeur_window <= 768){
-        menu_principal.forEach(element =>{
+
+        if(navigation_secondaire.classList.contains("afficher_barre_nav")){
             //On ferme également la barre secondaire si elle est ouverte
-            navigation_secondaire.classList.remove("afficher_barre_nav");
-            element.classList.toggle("active");
-        });
+            cacher_barre();
+            setTimeout(() => {
+                menu_principal.forEach(element =>{
+                    element.classList.toggle("active");
+                });
+            }, 650);
+        }else{
+            menu_principal.forEach(element =>{
+                element.classList.toggle("active");
+            });
+
+        }
     };
 })
